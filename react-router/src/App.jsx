3f25ebbf01eb11ddd6ef10803a875433f78af384
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-15 15:02:11 +0200

    [UPD] importate le rotte in app

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+
+import Homepage from './components/Homepage'
+import AboutUs from './components/AboutUs'
+import Products from './components/Products'
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Homepage} />
+          <Route path="/about" Component={AboutUs} />
+          <Route path="/products" Component={Products} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
