0335d3e074f8a09fba0dddc47e637f589fff4919
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-15 15:52:59 +0200

    [UPD] aggiunti e ciclati prodotti

--- react-router/src/components/Products.jsx
+++ react-router/src/components/Products.jsx
@@ -25,6 +25,19 @@ const Products = () => {
         <>
             <NavBar />
             <h1>Products</h1>
+            <div className='products'>
+                {products.map((product) => {
+                    return (
+                        <div className='product' key={product.id}>
+                            <img src={product.image} alt={product.title} />
+                            <h2>{product.title}</h2>
+                            <p>{product.description}</p>
+                            <p>${product.price}</p>
+                            <NavLink to={`/products/${product.id}`}>View Product</NavLink>
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
