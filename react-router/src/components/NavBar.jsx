c866be1bc507331a618aed164f211a608232e826
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-15 15:13:29 +0200

    [UPD] creata navbar con la classe active

--- react-router/src/components/NavBar.jsx
+++ react-router/src/components/NavBar.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
+
+const links = [
+    {
+        path: '/',
+        label: 'Homepage',
+    },
+    {
+        path: '/about',
+        label: 'About',
+    },
+    {
+        path: '/products',
+        label: 'Prodotti',
+    },
+]
 
 const NavBar = () => {
     return (
-        <div>NavBar</div>
+        <nav>
+            <ul>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <NavLink
+                            to={link.path}
+                            className={({ isActive }) =>
+                                isActive ? 'custom-link custom-link-active' : 'custom-link'
+                            }
+                        >
+                            {link.label}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     )
 }
 
