01e3a1b7c15057c979b3c3a0ef354cff5f5d6b29
ErikaArci99 <erikaarci1999@outlook.it>
2025-05-19 15:39:01 +0200

    [UPD] dichiarati parametri e funzioni per recuperare il prodotto tramite id

--- react-router/src/components/SingleProduct.jsx
+++ react-router/src/components/SingleProduct.jsx
@@ -3,8 +3,28 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
-export const SingleProduct = () => {
+const SingleProduct = () => {
+    // mi dichiaro la variabile id destrutturando cio che mi restituisce il metodo useParams cioe un oggetto con i parametri passati tramite url
+    const { id } = useParams();
+
+    // dichiaro la variabile di stato che conterra il prodotto
+    const [product, setProduct] = useState({});
+
+    // funzione che mi recupera le informazioni del post tramite parametro
+    const getProductsDetail = () => {
+        axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
+            setProduct(resp.data);
+        });
+    };
+
+    useEffect(() => {
+        getProductsDetail();
+    }, []);
+
+
     return (
         <div>SingleProduct</div>
     )
 }
+
+export default SingleProduct;
